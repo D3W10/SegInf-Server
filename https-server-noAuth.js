8ccbf6a65edae67db6eef76e5db7150385ba5912
Daniel Nunes <danyribeironunes15@gmail.com>
2024-11-28 11:54:56 +0000

    feat: Initial commit

---
+++ https-server-noAuth.js
@@ -0,0 +1,25 @@
+import { createServer } from "https";
+import fs from "fs";
+import express from "express";
+
+const PORT = 4433;
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log(
+        req.socket.remoteAddress
+        + " " + req.method
+        + " " + req.url);
+    res.send("<html><body>Secure Hello World with node.js</body></html>");
+});
+
+const options = {
+    key: fs.readFileSync("./keys/key.pem"),
+    cert: fs.readFileSync("./keys/cert.pem"),
+    ca: fs.readFileSync("./keys/CA2.pem")
+};
+
+const server = createServer(options, app);
+server.listen(PORT, () => {
+    console.log("Server started at port " + PORT);
+});
